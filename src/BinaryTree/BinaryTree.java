28855bd77728eed55091299592683819015fdee7
GIL <gil.00000032610@unicap.br>
2022-12-07 14:20:49 -0300

    first commit

---
+++ src/BinaryTree/BinaryTree.java
@@ -0,0 +1,135 @@
+package BinaryTree;
+
+public class BinaryTree<T extends Comparable<T>> {
+    private Node<T> root;
+
+    public boolean isEmpty() {
+        if (this.root == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void insert(T content) {
+        Node<T> aNode = new Node<>(content);
+        if (this.isEmpty()) {
+            this.root = aNode;
+        } else {
+            this.root.insertNode(content);
+        }
+    }
+
+    public Node<T> getRoot() {
+        return this.root;
+    }
+
+    public void setRoot(Node<T> root) {
+        this.root = root;
+    }
+
+    public void inOrder() {
+        if (this.isEmpty()) {
+            System.out.println("Arvore vazia");
+        } else {
+            percorrerEmOrdem(this.root);
+        }
+    }
+
+    private void percorrerEmOrdem(Node<T> root2) {
+        if (root2 != null) {
+            percorrerEmOrdem(root2.getLeft());
+            System.out.println(root2.getContent());
+            percorrerEmOrdem(root2.getRight());
+        }
+    }
+
+    public void inserir(T content) {
+        Node<T> aux, novo;
+        aux = this.root;
+        novo = new Node<>(content);
+        if (this.isEmpty()) {
+            this.root = novo;
+            return;
+        }
+        while (true) {
+            if (content.compareTo(aux.getContent()) == 0) {
+                System.out.println("Repeated");
+                break;
+            } else if (content.compareTo(aux.getContent()) < 0) { // inserção no lado esquerdo
+                if (aux.getLeft() != null) {
+                    aux = aux.getLeft();
+                } else {
+                    // inserir
+                    aux.setLeft(novo);
+                    break;
+                }
+            } else {// inserção no lado direito
+                if (aux.getRight() != null) {
+                    aux = aux.getRight();
+                } else {
+                    // inserir
+                    aux.setRight(novo);
+                    break;
+                }
+            }
+        }
+    }
+
+    public void preOrdem() {
+        if (this.isEmpty()) {
+            System.out.println("Empty tree");
+        } else {
+            percorrerPreOrdem(this.root);
+        }
+    }
+
+    private void percorrerPreOrdem(Node<T> root2) {
+        if (root != null) {
+            System.out.println(root.getContent());
+            percorrerPreOrdem(root2.getLeft());
+            percorrerPreOrdem(root2.getRight());
+        }
+    }
+
+    public void remove(T content) {
+        if (this.isEmpty()) {
+            System.out.println("Empty tree!");
+        } else {
+            this.root = this.removeNode(this.root, content);
+        }
+    }
+
+    private Node<T> removeNode(Node<T> root, T content) {
+        if (root != null) {
+            if (content.compareTo(root.getContent()) == 0) {
+                Node<T> father, son;
+                if (root.getLeft() == null && root.getRight() == null) {
+                    root = null;
+                } else if (root.getLeft() == null) {
+                    root = root.getRight();
+                } else if (root.getRight() == null) {
+                    root = root.getLeft();
+                } else {
+                    father = root;
+                    son = root.getLeft();
+                    while (son.getRight() != null) {
+                        father = son;
+                        son = son.getRight();
+                    }
+                    father.setRight(son.getLeft());
+                    root.setContent(son.getContent());
+                }
+
+            } else if (content.compareTo(root.getContent()) < 0) {
+                root.setLeft(removeNode(root.getLeft(), content));
+            } else {
+                root.setRight(removeNode(root.getRight(), content));
+            }
+        } else {
+            return root;
+        }
+        return root;
+    }
+
+}
---
+++ src/Produtos/Produto.java
@@ -0,0 +1,64 @@
+package Produtos;
+
+public class Produto implements Comparable<Produto> {
+    private String code;
+    private String descrip;
+    private double price;
+    private int invent;
+
+    public Produto(String code, String descrip, double price, int invent) {
+        this.code = code;
+        this.price = price;
+        this.invent = invent;
+        this.descrip = descrip;
+    }
+    public Produto(String code, String descrip){
+        this.descrip = descrip;
+        this.code = code;
+    }
+
+    public Produto(String code) {
+        this.code = code;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setInvent(int invent) {
+        this.invent = invent;
+    }
+
+    public void setDescrip(String descrip) {
+        this.descrip = descrip;
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public int getInvent() {
+        return this.invent;
+    }
+
+    public String getDescrip() {
+        return this.descrip;
+    }
+
+    @Override
+    public String toString() {
+        return this.code + " " + this.descrip + " " + this.price + " " + this.invent;
+    }
+
+    @Override
+    public int compareTo(Produto p) {
+        int result;
+        result = this.getCode().compareTo(p.getCode());
+        return result;
+    }
+
+}
