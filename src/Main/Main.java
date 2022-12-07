905776835c16a6a16d41b5588dd4d8ca2e2fcbb1
GIL <gil.00000032610@unicap.br>
2022-12-07 15:51:12 -0300

    Main concluded

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,14 +1,53 @@
 package Main;
 
+import java.util.Scanner;
+
+import ProductManagement.CadastroProdutos;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         int op, invent;
         double price;
         String code, descrip;
+        CadastroProdutos cp = new CadastroProdutos();
         do {
             showMenu();
             op = Integer.parseInt(s.nextLine());
-            
+            switch (op) {
+                case 1:
+                    cp.registerProduct();
+                    break;
+                case 2:
+                    cp.exibirProdutos();
+                    break;
+                case 3:
+                    System.out.println("Informe o código do produto:");
+                    code = s.nextLine();
+                    cp.alterarPreço(code);
+                    break;
+                case 4:
+                    System.out.println("Informe o código do produto:");
+                    code = s.nextLine();
+                    cp.changeInventory(code);
+                    break;
+                case 5:
+                    System.out.println("Informe o código do produto:");
+                    code = s.nextLine();
+                    cp.showProduct(code);
+                    break;
+                case 6:
+                    System.out.println("Informe o código do produto:");
+                    code = s.nextLine();
+                    cp.removeProduct(code);
+                    break;
+                case 0:
+                    System.out.println("Volte sempre!");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
 
         } while (op != 0);
     }
