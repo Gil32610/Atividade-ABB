c9a49b42bd78a59b8606103c955cf759994a4368
GIL <gil.00000032610@unicap.br>
2022-12-07 14:33:29 -0300

    Class CadastroProdutos created

---
+++ src/ProductManagement/CadastroProdutos.java
@@ -0,0 +1,13 @@
+package ProductManagement;
+
+import BinaryTree.*;
+import Produtos.*;
+
+public class CadastroProdutos {
+    private BinaryTree<Produto> Cadastro;
+
+    public CadastroProdutos() {
+        this.Cadastro = new BinaryTree<Produto>();
+    }
+    
+}
